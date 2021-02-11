d8d51422576e29eb151b311fd92908b67c4a7d61
Thomasrm23 <tom.remy23@gmail.com>
2021-02-11 21:41:45 +0100

    reda prend le relai

--- src/main/Island.java
+++ src/main/Island.java
@@ -1,5 +1,6 @@
 package main;
 
+import event.Event;
 
 public class Island {
 	private int pctAgriculture;
@@ -77,7 +78,36 @@ public class Island {
 	public void setIndustryUnity(int industryUnity) {
 		this.industryUnity = industryUnity;
 	}
-	
+	public Island from(String[] s) {
+		
+		int capitalistSatisfaction = Integer.parseInt(s[0].strip());
+		int communistSatisfaction = Integer.parseInt(s[1].strip());
+		int ecologistSatisfaction = Integer.parseInt(s[2].strip());
+		int liberalSatisfaction = Integer.parseInt(s[3].strip());
+		int loyalistSatisfaction = Integer.parseInt(s[4].strip());
+		int militaristSatisfaction = Integer.parseInt(s[5].strip());
+        int nationalistSatisfaction = Integer.parseInt(s[6].strip());
+        int religiousSatisfaction = Integer.parseInt(s[7].strip());
+        int pctAgriculture = Integer.parseInt(s[8].strip());
+        int pctIndustry = Integer.parseInt(s[9].strip());
+        int treasure = Integer.parseInt(s[10].strip());
+        int capitalistMember = Integer.parseInt(s[11].strip());
+        int communistMember = Integer.parseInt(s[12].strip());
+        int ecologistMember = Integer.parseInt(s[13].strip());
+        int liberalMember = Integer.parseInt(s14].strip());
+        int loyalistMember = Integer.parseInt(s[15].strip());
+        int militaristMember = Integer.parseInt(s[16].strip());
+        int nationalistMember = Integer.parseInt(s[17].strip());
+        int religiousMember = Integer.parseInt(s[18].strip());
+		
+
+		int eventId = Integer.parseInt(strEventId);
+		int type = Integer.parseInt(strType);
+
+		Island islan = new Event(eventId, question, eventId, type);
+
+		return island;
+	}
 	
 
 }
